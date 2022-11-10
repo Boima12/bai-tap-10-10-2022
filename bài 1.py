23527cae1757e0448d0aa6086f385fe98524ce9c
Boima <boimache27@gmail.com>
2022-11-10 22:35:56 +0700

    hello ohalo?

---
+++ bài 1.py
@@ -0,0 +1,15 @@
+# bài 1 tính tổng các số nguyên lẻ trong dãy
+n = int(input())
+a = []
+t = 0
+
+for i in range(0,n):
+    a.append(int(input()))
+
+for i in a:
+    if i % 2 != 0:
+        t += i
+
+print("tổng số nguyên lẻ trong dãy là:",t)
+
+#Cao Hoang Phuoc Bao - Boima
